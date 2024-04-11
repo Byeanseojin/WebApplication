014057391eb85ad786a7a5d1394b0b680627635d
Byeanseojin <sjb12365@gmail.com>
2024-04-11 11:20:15 +0900

    thymeleaf의 속성들 inline, block, 하이퍼링크, 숫자, 날짜형식

--- src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
+++ src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,7 +21,7 @@ public class SampleController {
 
     }
 
-    @GetMapping({"/ex2","/ex2_1"})
+    @GetMapping({"/ex2","/ex2_1","/exblock", "/exLink"})
     public void exModel(Model model) {
         List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i ->{
             SampleDTO dto = SampleDTO.builder()
@@ -33,5 +34,23 @@ public class SampleController {
         }).collect(Collectors.toList());
 
         model.addAttribute("list",list);
+    }
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttirbutes){
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First..100")
+                .last("Last..100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttirbutes.addFlashAttribute("result","success");
+        redirectAttirbutes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(){
+
     }
 }
