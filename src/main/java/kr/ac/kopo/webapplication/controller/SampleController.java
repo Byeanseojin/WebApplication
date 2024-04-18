db08b0149cca32a2dd396863fe631fb5c603fa09
Byeanseojin <sjb12365@gmail.com>
2024-04-18 09:45:50 +0900

    매개변수(parameter) fragment 처리

--- src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
+++ src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
@@ -49,7 +49,7 @@ public class SampleController {
         return "redirect:/sample/ex3";
     }
 
-    @GetMapping({"/ex3", "/exLayout1"})
+    @GetMapping({"/ex3", "/exLayout1", "/exLayout2"})
     public void ex3(){
 
     }
